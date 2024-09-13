fdc5f687170273358e41f9eda205a84eaaeb7f0a
Huzaifa Naseer Khan <huzaifanaseerkhan02@gmail.com>
2024-09-13 17:18:23 +0500

    Completed game screen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,14 +1,10 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 
-const PrimaryButton = ({ children }) => {
-  function handlePress() {
-    console.log('Button pressed')
-  }
-
+const PrimaryButton = ({ children, onPress }) => {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        onPress={handlePress}
+        onPress={onPress}
         style={({ pressed }) =>
           pressed
             ? [styles.buttonInnerContainer, styles.pressed]
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,34 +1,83 @@
-import { View, TextInput, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet, Alert } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
+import { useState } from 'react'
 
-const StartGameScreen = () => {
+const StartGameScreen = ({ onPickNumber }) => {
+  const [enteredNumber, setEnteredNumber] = useState('')
+
+  function numberInputHandler(inputText) {
+    setEnteredNumber(inputText)
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('')
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber)
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid Number!',
+        'Number has to be a number between 1 and 99.',
+        [
+          {
+            text: 'Okay',
+            style: 'destructive',
+            onPress: resetInputHandler
+          }
+        ]
+      )
+      return
+    }
+    onPickNumber(chosenNumber)
+  }
+
   return (
     <View style={styles.inputContainer}>
       <TextInput
         style={styles.numberInput}
         maxLength={2}
         keyboardType='number-pad'
+        onChangeText={numberInputHandler}
         autoCorrect={false}
         autoCapitalize='none'
+        value={enteredNumber}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     borderRadius: 8,
     elevation: 4,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 6
+  },
+  buttonContainer: {
+    flex: 1
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: 16
   },
   numberInput: {
     height: 50,
